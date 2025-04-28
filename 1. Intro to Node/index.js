f0b793337a989b17ed3ef2ea97f3149c4bba35e9
Hassan-Jamal <hassanjamal6577987@gmail.com>
2025-04-28 17:16:31 +0500

    Initial commit with 2 folders and Node Module System files

---
+++ 1. Intro to Node/index.js
@@ -0,0 +1,10 @@
+console.log("Hi Hassan");
+
+array = [1,2,3,4];
+console.log(array," array");
+
+setTimeout(()=>{
+    console.log("This line is delayed by 2 second.");
+},2000);
+
+console.log("This is last line of the code.");
